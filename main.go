6741ec0dfbfdeb8b7c2683c3afc2af3243016aa5
Rakhesh Prasaath B <rakheshprasaath@ayoconnect.id>
2025-03-31 12:01:43 +0530

    added db connection

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/gofiber/fiber/v2"
@@ -9,8 +12,24 @@ import (
 	"github.com/rakheshprasaath/trade-book.git/database"
 	"github.com/rakheshprasaath/trade-book.git/routes"
 )
+func getPublicIP() string {
+	resp, err := http.Get("https://api64.ipify.org")
+	if err != nil {
+		return "Error fetching public IP"
+	}
+	defer resp.Body.Close()
+
+	ip, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "Error reading response"
+	}
+
+	return string(ip)
+}
 
 func main() {
+	fmt.Println("Public IP:", getPublicIP())
+
 	database.Connect()
 
 	err := godotenv.Load()
